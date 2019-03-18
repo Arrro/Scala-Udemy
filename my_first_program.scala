5264ec58021205597f8c3992b4a3c29c336e7a09
Steve <tyler.steve@teradata.com>
2019-03-18 11:34:47 -0700

    Added test message

--- my_first_program.scala
+++ my_first_program.scala
@@ -4,4 +4,6 @@ val st: String = "This is a long string"
 
 st.slice (10,14)
 
-printf("A string %s ,an integer %d ,a float %f", "Hi", 10, 12.345) 
+printf("A string %s ,an integer %d ,a float %f", "Hi", 10, 12.345)
+
+printf("Testing for github commit to master")
