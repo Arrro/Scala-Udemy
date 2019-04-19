d7cff9a84f0a23ebc46bd76264e4f723c4a4fe96
Steve <tyler.steve@teradata.com>
2019-04-19 10:57:15 -0700

    Flow Control

--- my_first_program.scala
+++ my_first_program.scala
@@ -43,12 +43,47 @@ var list3 = List(2,3,1,4,5,6,6,1,2)
 val test_list2 = List(6,7)
 val test_list = List(1,2,3,4,5) // Find if te lsit contains the #3
 test_list.indexOf(3) // Answer \\ test_list.contains(3) wil work
+test_list.contains(3) // Also works here to return a boolean
 // How can you add a previous list
 list.sum // Should be 15
 // Create an array of all the odd number from 0 to 15
-var test_array = (1,3,5,7,9,11,13,15)
+Array.Range(1,16,2)
 // What are the unique elements of list3
 list3.distinct // Will return the unique numbers in the list
+list3.toSet // This can also return unique values as a set returns non-duplicates
 var test_map = collection.mutable.Map(("Sammy",3),("Frankie",7),("John",45))
 test_map.keys // Prints out all the keys
-test_map += ("Mike",27) // Add a vlaue to the map
+test_map += (("Mike",27)) // Add a vlaue to the map
+test_map =+ ("Mike" -> 27) // Does the same as above
+
+// Flow Control
+val x = "zzz"
+
+if (x.endsWith("o")){
+    println("Thevlaue of x ends with o")
+}else{
+    println("The value of x does not end with o")
+}
+
+val person = "George"
+
+if (person == "Sammy"){
+    println("Welcome Sammy!")
+}else if(person == "George"){
+    println("Welcome George!")
+}else{
+    println("What is your name?")
+}
+
+// Logical Operators (And, Or, Not)
+// And
+println((1 == 2) && (2==2)) // Returns false as both are not true, both must be true to be true
+// Or
+println((1==2) || (2==2)) // One can be true for it to be true
+// NOT !
+println(!(1==1)) // Checking for inequality
+
+// For Loops
+for (item <- List(1,2,3)){
+    println(int)
+}
